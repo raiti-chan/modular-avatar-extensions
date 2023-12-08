ee10da007d3055edc734d2bb203bb17c14f44196
らいちちゃん <rusuke@t-net.ne.jp>
2023-12-08 15:32:18 +0900

    support ndmf

---
+++ Packages/raitichan.com.modular-avatar.extensions/Editor/ControllerFactories/ControllerFactoryBase.cs
@@ -0,0 +1,19 @@
+﻿using nadena.dev.ndmf;
+using raitichan.com.modular_avatar.extensions.Modules;
+using UnityEngine;
+
+namespace raitichan.com.modular_avatar.extensions.Editor.ControllerFactories {
+	public interface IControllerFactory {
+		void PreProcess(BuildContext context);
+		RuntimeAnimatorController CreateController(BuildContext context);
+		void PostProcess(BuildContext context);
+	}
+	
+	public abstract class ControllerFactoryBase<T> : IRuntimeAnimatorFactory<T>, IControllerFactory where T : MAExAnimatorGeneratorModuleBase {
+		public abstract void PreProcess(BuildContext context);
+		public abstract RuntimeAnimatorController CreateController(BuildContext context);
+		public abstract void PostProcess(BuildContext context);
+
+		public T Target { get; set; }
+	}
+}
